97d5a3491ff8bf17f867df2ea4e82906b3f64b97
lnsun <57457122+lnsun@users.noreply.github.com>
2020-05-28 16:53:48 -0400

    fix Issue376 crush: receiver-sensitive poly resolution

--- framework/src/main/java/org/checkerframework/framework/type/AnnotatedTypeFactory.java
+++ framework/src/main/java/org/checkerframework/framework/type/AnnotatedTypeFactory.java
@@ -2024,10 +2024,11 @@ public class AnnotatedTypeFactory implements AnnotationProvider {
             ExpressionTree tree, ExecutableElement methodElt, AnnotatedTypeMirror receiverType) {
 
         AnnotatedExecutableType memberType = getAnnotatedType(methodElt); // get unsubstituted type
+
+        methodFromUsePreSubstitution(tree, memberType);
         if (viewpointAdapter != null) {
             viewpointAdapter.viewpointAdaptMethod(receiverType, methodElt, memberType);
         }
-        methodFromUsePreSubstitution(tree, memberType);
 
         AnnotatedExecutableType methodType =
                 AnnotatedTypes.asMemberOf(types, this, receiverType, methodElt, memberType);
@@ -2166,10 +2167,11 @@ public class AnnotatedTypeFactory implements AnnotationProvider {
         addComputedTypeAnnotations(tree, type);
 
         AnnotatedExecutableType con = getAnnotatedType(ctor); // get unsubstituted type
+
+        constructorFromUsePreSubstitution(tree, con);
         if (viewpointAdapter != null) {
             viewpointAdapter.viewpointAdaptConstructor(type, ctor, con);
         }
-        constructorFromUsePreSubstitution(tree, con);
 
         con = AnnotatedTypes.asMemberOf(types, this, type, ctor, con);
 
