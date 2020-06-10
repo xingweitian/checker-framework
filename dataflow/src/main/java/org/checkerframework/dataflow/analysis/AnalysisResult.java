80b432e3985772a78c18aa73debf5ce9a55cd274
xingweitian <xingweitian@gmail.com>
2020-06-10 18:29:00 -0400

    Fix javadoc.

--- dataflow/src/main/java/org/checkerframework/dataflow/analysis/AnalysisResult.java
+++ dataflow/src/main/java/org/checkerframework/dataflow/analysis/AnalysisResult.java
@@ -166,9 +166,6 @@ public class AnalysisResult<V extends AbstractValue<V>, S extends Store<S>> {
      * @return the abstract value for {@link Node} {@code n}, or {@code null} if no information is
      *     available Returns the abstract value for {@link Node} {@code n}, or {@code null} if no
      *     information is available.
-     * @param n a node
-     * @return the abstract value for {@link Node} {@code n}, or {@code null} if no information is
-     *     available
      */
     public @Nullable V getValue(Node n) {
         return nodeValues.get(n);
